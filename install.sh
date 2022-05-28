b3fe765087a786a8a9c1e741385236ea152b84c2
AkashiCoin <55268546+AkashiCoin@users.noreply.github.com>
2022-05-28 23:55:43 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ export PATH
 
 update_shell_url="https://raw.githubusercontent.com/zhenxun-org/zhenxun_bot-deploy/master/install.sh"
 zhenxun_url="https://github.com/HibiKier/zhenxun_bot.git"
-work_dir="/home"
+WORK_DIR="/home"
+TMP_DIR="$(mktemp -d)"
 python_v="python3.8"
 which python3.9 && python_v="python3.9"
 sh_ver="1.0.4"
@@ -41,11 +42,11 @@ check_sys() {
 }
 
 check_installed_zhenxun_status() {
-  [[ ! -e "${work_dir}/zhenxun_bot/bot.py" ]] && echo -e "${Error} zhenxun_bot 没有安装，请检查 !" && exit 1
+  [[ ! -e "${WORK_DIR}/zhenxun_bot/bot.py" ]] && echo -e "${Error} zhenxun_bot 没有安装，请检查 !" && exit 1
 }
 
 check_installed_cqhttp_status() {
-  [[ ! -e "${work_dir}/go-cqhttp/go-cqhttp" ]] && echo -e "${Error} go-cqhttp 没有安装，请检查 !" && exit 1
+  [[ ! -e "${WORK_DIR}/go-cqhttp/go-cqhttp" ]] && echo -e "${Error} go-cqhttp 没有安装，请检查 !" && exit 1
 }
 
 check_pid_zhenxun() {
@@ -79,9 +80,9 @@ Installation_dependency() {
         sudo yum -y update
         sudo yum install -y git fontconfig mkfontscale epel-release wget vim curl zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gcc make libffi-devel
         if  ! which python3.8 && ! which python3.9; then
-            wget https://mirrors.huaweicloud.com/python/3.9.10/Python-3.9.10.tgz -O /tmp/Python-3.9.10.tgz && \
-                tar -zxf /tmp/Python-3.9.10.tgz -C /tmp/ &&\
-                cd /tmp/Python-3.9.10 --with-ensurepip=install && \
+            wget https://mirrors.huaweicloud.com/python/3.9.10/Python-3.9.10.tgz -O ${TMP_DIR}/Python-3.9.10.tgz && \
+                tar -zxf ${TMP_DIR}/Python-3.9.10.tgz -C ${TMP_DIR}/ &&\
+                cd ${TMP_DIR}/Python-3.9.10 --with-ensurepip=install && \
                 ./configure && \
                 make -j $(cat /proc/cpuinfo |grep "processor"|wc -l) && \
                 sudo make altinstall
@@ -95,18 +96,18 @@ Installation_dependency() {
         sudo /usr/pgsql-13/bin/postgresql-13-setup initdb
         sudo systemctl enable postgresql-13
         sudo systemctl start postgresql-13
-        cat > /tmp/sql.sql <<-EOF
+        cat > ${TMP_DIR}/sql.sql <<-EOF
 CREATE USER zhenxun WITH PASSWORD 'zxpassword';
 CREATE DATABASE zhenxun OWNER zhenxun;
 EOF
-        su postgres -c "psql -f /tmp/sql.sql"
+        su postgres -c "psql -f ${TMP_DIR}/sql.sql"
     elif [[ ${release} == "debian" ]]; then
         sudo apt-get update
         sudo apt-get install -y wget ttf-wqy-zenhei xfonts-intl-chinese wqy* build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev
         if  ! which python3.8 && ! which python3.9;then
-            wget https://mirrors.huaweicloud.com/python/3.9.10/Python-3.9.10.tgz -O /tmp/Python-3.9.10.tgz && \
-                tar -zxf /tmp/Python-3.9.10.tgz -C /tmp/ &&\
-                cd /tmp/Python-3.9.10 && \
+            wget https://mirrors.huaweicloud.com/python/3.9.10/Python-3.9.10.tgz -O ${TMP_DIR}/Python-3.9.10.tgz && \
+                tar -zxf ${TMP_DIR}/Python-3.9.10.tgz -C ${TMP_DIR}/ &&\
+                cd ${TMP_DIR}/Python-3.9.10 && \
                 ./configure --with-ensurepip=install && \
                 make -j $(cat /proc/cpuinfo |grep "processor"|wc -l) && \
                 sudo make altinstall
@@ -133,18 +134,18 @@ EOF
             libasound2
         ${python_v} <(curl -s -L https://bootstrap.pypa.io/get-pip.py) || echo -e "${Tip} pip 安装出错..."
         /etc/init.d/postgresql start
-        cat > /tmp/sql.sql <<-EOF
+        cat > ${TMP_DIR}/sql.sql <<-EOF
 CREATE USER zhenxun WITH PASSWORD 'zxpassword';
 CREATE DATABASE zhenxun OWNER zhenxun;
 EOF
-        su postgres -c "psql -f /tmp/sql.sql"
+        su postgres -c "psql -f ${TMP_DIR}/sql.sql"
     elif [[ ${release} == "ubuntu" ]]; then
         sudo apt-get update
         sudo apt-get install -y software-properties-common ttf-wqy-zenhei ttf-wqy-microhei fonts-arphic-ukai fonts-arphic-uming
         sudo fc-cache -f -v
         echo -e "\n" | sudo add-apt-repository ppa:deadsnakes/ppa
         if  ! which python3.8 && ! which python3.9;then
-            sudo apt-get install -y python3.9 python3.9-pip
+            sudo apt-get install -y python3.9-full
             python_v="python3.9"
         fi
         sudo apt-get install -y \
@@ -168,11 +169,11 @@ EOF
             libasound2
         ${python_v} <(curl -s -L https://bootstrap.pypa.io/get-pip.py) || echo -e "${Tip} pip 安装出错..."
         /etc/init.d/postgresql start
-        cat > /tmp/sql.sql <<-EOF
+        cat > ${TMP_DIR}/sql.sql <<-EOF
 CREATE USER zhenxun WITH PASSWORD 'zxpassword';
 CREATE DATABASE zhenxun OWNER zhenxun;
 EOF
-        su postgres -c "psql -f /tmp/sql.sql"
+        su postgres -c "psql -f ${TMP_DIR}/sql.sql"
     elif [[ ${release} == "archlinux" ]]; then
         pacman -Sy python python-pip unzip --noconfirm
     fi
@@ -191,25 +192,25 @@ check_arch() {
 }
 
 Download_zhenxun_bot() {
-    cd "/tmp" || exit 1
+    cd "${TMP_DIR}" || exit 1
     echo -e "${Info} 开始下载最新版 zhenxun_bot ..."
     git clone "${ghproxy}${zhenxun_url}" -b main || (echo -e "${Error} zhenxun_bot 下载失败 !" && exit 1)
     echo -e "${Info} 开始下载最新版 go-cqhttp ..."
     gocq_version=$(wget -qO- -t1 -T2 "https://api.github.com/repos/Mrs4s/go-cqhttp/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
     wget -qO- "${ghproxy}https://github.com/Mrs4s/go-cqhttp/releases/download/${gocq_version}/go-cqhttp_$(uname -s)_${arch}.tar.gz" -O go-cqhttp.tar.gz || (echo -e "${Error} go-cqhttp 下载失败 !" && exit 1)
-    cd "${work_dir}" || exit 1
-    mv "/tmp/zhenxun_bot" ./
+    cd "${WORK_DIR}" || exit 1
+    mv "${TMP_DIR}/zhenxun_bot" ./
     mkdir -p "go-cqhttp"
-    tar -zxf "/tmp/go-cqhttp.tar.gz" -C ./go-cqhttp/
+    tar -zxf "${TMP_DIR}/go-cqhttp.tar.gz" -C ./go-cqhttp/
     echo -e "${info} 开始下载抽卡相关资源..."
-    if [[ -e "${work_dir}/zhenxun_bot/draw_card" ]]; then
+    if [[ -e "${WORK_DIR}/zhenxun_bot/draw_card" ]]; then
         echo -e "${info} 抽卡资源文件已存在，跳过下载"
     else
         SOURCE_URL=https://pan.yropo.top/source/zhenxun/
         wget ${SOURCE_URL}data_draw_card.tar.gz -qO ~/.cache/data_draw_card.tar.gz \
             && wget ${SOURCE_URL}img_draw_card.tar.gz -qO ~/.cache/img_draw_card.tar.gz \
-            && tar -zxf ~/.cache/data_draw_card.tar.gz -C ${work_dir}/zhenxun_bot/ \
-            && tar -zxf ~/.cache/img_draw_card.tar.gz -C ${work_dir}/zhenxun_bot/ \
+            && tar -zxf ~/.cache/data_draw_card.tar.gz -C ${WORK_DIR}/zhenxun_bot/ \
+            && tar -zxf ~/.cache/img_draw_card.tar.gz -C ${WORK_DIR}/zhenxun_bot/ \
             && rm -rf ~/.cache/*.tar.gz
     fi
 }
@@ -218,7 +219,7 @@ Set_config_admin() {
     echo -e "${Info} 请输入管理员QQ账号(也就超级用户账号):[QQ]"
     read -erp "管理员QQ:" admin_qq
     [[ -z "$admin_qq" ]] && admin_qq=""
-    cd ${work_dir}/zhenxun_bot && \
+    cd ${WORK_DIR}/zhenxun_bot && \
       sed -i "s/SUPERUSERS.*/SUPERUSERS=[\"$admin_qq\"]/g" .env.dev && \
       sed -i "s/PORT.*/PORT = 14514/g" .env.dev || \
       echo -e "${Error} 配置文件不存在！请检查zhenxun_bot是否安装正确!"
@@ -229,15 +230,15 @@ Set_config_bot() {
     echo -e "${Info} 请输入Bot QQ账号:[QQ]"
     read -erp "Bot QQ:" bot_qq
     [[ -z "$bot_qq" ]] && bot_qq=""
-    cd ${work_dir}/go-cqhttp && sed -i "s/uin:.*/uin: $bot_qq/g" config.yml || echo -e "${Error} 配置文件不存在！请检查go-cqhttp是否安装正确!"
+    cd ${WORK_DIR}/go-cqhttp && sed -i "s/uin:.*/uin: $bot_qq/g" config.yml || echo -e "${Error} 配置文件不存在！请检查go-cqhttp是否安装正确!"
     echo -e "${info} 设置成功!Bot QQ: ${bot_qq}"
 }
 
 Set_config() {
-    if [[ -e "${work_dir}/go-cqhttp/config.yml" ]]; then
+    if [[ -e "${WORK_DIR}/go-cqhttp/config.yml" ]]; then
         echo -e "${info} go-cqhttp 配置文件已存在，跳过生成"
     else
-        cd ${work_dir}/go-cqhttp && echo -e "3\n" | ./go-cqhttp > /dev/null 2>&1
+        cd ${WORK_DIR}/go-cqhttp && echo -e "3\n" | ./go-cqhttp > /dev/null 2>&1
         sudo sed -i 's|universal:.*|universal: ws://localhost:14514/onebot/v11/ws|g' config.yml
     fi
     Set_config_bot
@@ -250,7 +251,7 @@ Start_zhenxun_bot() {
     check_installed_zhenxun_status
     check_pid_zhenxun
     [[ -n ${PID} ]] && echo -e "${Error} zhenxun_bot 正在运行，请检查 !" && exit 1
-    cd ${work_dir}/zhenxun_bot
+    cd ${WORK_DIR}/zhenxun_bot
     nohup ${python_v} bot.py >> zhenxun_bot.log 2>&1 &
     echo -e "${Info} zhenxun_bot 开始运行..."
 }
@@ -269,18 +270,18 @@ Restart_zhenxun_bot() {
 }
 
 View_zhenxun_log() {
-    tail -f -n 100 ${work_dir}/zhenxun_bot/zhenxun_bot.log
+    tail -f -n 100 ${WORK_DIR}/zhenxun_bot/zhenxun_bot.log
 }
 
 Set_config_zhenxun() {
-    vim ${work_dir}/zhenxun_bot/configs/config.yaml
+    vim ${WORK_DIR}/zhenxun_bot/configs/config.yaml
 }
 
 Start_cqhttp() {
     check_installed_cqhttp_status
     check_pid_cqhttp
     [[ -n ${PID} ]] && echo -e "${Error} go-cqhttp 正在运行，请检查 !" && exit 1
-    cd ${work_dir}/go-cqhttp
+    cd ${WORK_DIR}/go-cqhttp
     nohup ./go-cqhttp -faststart >> go-cqhttp.log 2>&1 &
     echo -e "${Info} go-cqhttp 开始运行..."
     echo -e "${info} 请扫描二维码登录 bot，bot 账号登录完成后，使用Ctrl + C退出 !"
@@ -301,20 +302,20 @@ Restart_cqhttp() {
 }
 
 View_cqhttp_log() {
-    tail -f -n 100 ${work_dir}/go-cqhttp/go-cqhttp.log
+    tail -f -n 100 ${WORK_DIR}/go-cqhttp/go-cqhttp.log
 }
 
 Set_config_cqhttp() {
-    vim ${work_dir}/go-cqhttp/config.yml
+    vim ${WORK_DIR}/go-cqhttp/config.yml
 }
 
 
 Set_config_zhenxun() {
-    vim ${work_dir}/zhenxun_bot/configs/config.yaml
+    vim ${WORK_DIR}/zhenxun_bot/configs/config.yaml
 }
 
 Exit_cqhttp() {
-    cd ${work_dir}/go-cqhttp
+    cd ${WORK_DIR}/go-cqhttp
     rm -f session.token
     echo -e "${Info} go-cqhttp 账号已退出..."
     Stop_cqhttp
@@ -323,7 +324,7 @@ Exit_cqhttp() {
 }
 
 Set_dependency() {
-    cd ${work_dir}/zhenxun_bot
+    cd ${WORK_DIR}/zhenxun_bot
     Set_pip_Mirror
     ${python_v} -m pip install --ignore-installed -r ${ghproxy}https://raw.githubusercontent.com/zhenxun-org/zhenxun_bot-deploy/master/requirements.txt -i ${mirror_url}
     playwright install chromium
@@ -334,7 +335,7 @@ Uninstall_All() {
   read -erp "请选择 [y/n], 默认为 n:" uninstall_check
   [[ -z "${uninstall_check}" ]] && uninstall_check='n'
   if [[ ${uninstall_check} == 'y' ]]; then
-    cd ${work_dir}
+    cd ${WORK_DIR}
     check_pid_zhenxun
     [[ -z ${PID} ]] || kill -9 ${PID}
     echo -e "${Info} 开始卸载 zhenxun_bot..."
@@ -349,7 +350,7 @@ Uninstall_All() {
 }
 
 Install_zhenxun_bot() {
-    [[ -e "${work_dir}/zhenxun_bot/bot.py" ]] && echo -e "${Error} 检测到 zhenxun_bot 已安装 !" && exit 1
+    [[ -e "${WORK_DIR}/zhenxun_bot/bot.py" ]] && echo -e "${Error} 检测到 zhenxun_bot 已安装 !" && exit 1
     startTime=`date +%s`
     Set_ghproxy
     echo -e "${Info} 开始检查系统..."
@@ -366,7 +367,7 @@ Install_zhenxun_bot() {
     if [[ ${release} == "centos" ]]; then
         echo -e "${Info} CentOS 中文字体设置..."
         sudo mkdir -p /usr/share/fonts/chinese
-        sudo cp -r ${work_dir}/zhenxun_bot/resources/font /usr/share/fonts/chinese
+        sudo cp -r ${WORK_DIR}/zhenxun_bot/resources/font /usr/share/fonts/chinese
         cd /usr/share/fonts/chinese && mkfontscale
     fi
     endTime=`date +%s`
@@ -412,7 +413,7 @@ menu_cqhttp() {
 ————————————
  ${Green_font_prefix} 8.${Font_color_suffix} 退出 go-cqhttp 账号
  ${Green_font_prefix}10.${Font_color_suffix} 切换为 zhenxun_bot 菜单" && echo
-  if [[ -e "${work_dir}/go-cqhttp/go-cqhttp" ]]; then
+  if [[ -e "${WORK_DIR}/go-cqhttp/go-cqhttp" ]]; then
     check_pid_cqhttp
     if [[ -n "${PID}" ]]; then
       echo -e " 当前状态: go-cqhttp ${Green_font_prefix}已安装${Font_color_suffix} 并 ${Green_font_prefix}已启动${Font_color_suffix}"
@@ -487,7 +488,7 @@ menu_zhenxun() {
 ————————————
  ${Green_font_prefix} 8.${Font_color_suffix} 卸载 zhenxun_bot + go-cqhttp
  ${Green_font_prefix}10.${Font_color_suffix} 切换为 go-cqhttp 菜单" && echo
-  if [[ -e "${work_dir}/zhenxun_bot/bot.py" ]]; then
+  if [[ -e "${WORK_DIR}/zhenxun_bot/bot.py" ]]; then
     check_pid_zhenxun
     if [[ -n "${PID}" ]]; then
       echo -e " 当前状态: zhenxun_bot ${Green_font_prefix}已安装${Font_color_suffix} 并 ${Green_font_prefix}已启动${Font_color_suffix}"
